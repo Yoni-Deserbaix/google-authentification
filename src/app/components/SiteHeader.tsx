c6304f12cc60f7c5fe24e85e8c1293d2519a3e39
unknown <yonideserbaix@gmail.com>
2024-03-15 21:57:41 +0100

    update header

--- src/app/components/SiteHeader.tsx
+++ src/app/components/SiteHeader.tsx
@@ -1,3 +1,17 @@
 export default function SiteHeader() {
-  return <div>SiteHeader</div>;
+  return (
+    <header className="flex justify-between mx-4 max-w-7xl">
+      <div className="flex gap-4">
+        <p>Logo</p>
+        <p>Title</p>
+      </div>
+      <p>Search area</p>
+      <div className="flex gap-4">
+        <p>About</p>
+        <p>Services</p>
+        <p>Vision</p>
+        <p>ThemeToggle</p>
+      </div>
+    </header>
+  );
 }
