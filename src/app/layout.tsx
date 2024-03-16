e9879d4978ecf7230f0477ed4cb490e82a77ffea
unknown <yonideserbaix@gmail.com>
2024-03-16 16:30:09 +0100

    create header folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "./components/Header";
+import Header from "./components/Header/Header";
 import Footer from "./components/Footer";
-import {ThemeProvider} from "./components/ThemeProvider";
+import { ThemeProvider } from "./components/Header/ThemeProvider";
 import Blob from "./components/Blob";
 const inter = Inter({ subsets: ["latin"] });
 
