93235e4b83fb69667954a0bde6e7f5db05084d74
unknown <yonideserbaix@gmail.com>
2024-03-16 14:17:04 +0100

    create vision page

---
+++ src/app/vision/page.tsx
@@ -0,0 +1,10 @@
+export default function page() {
+  return (
+    <main className="mx-auto max-w-6xl px-6">
+      <div className="px-10 pt-10">
+        <h1 className="text-4xl font-extrabold">Vision Page</h1>
+        <p className="py-4 text-xl">Whats your vision</p>
+      </div>
+    </main>
+  );
+}
