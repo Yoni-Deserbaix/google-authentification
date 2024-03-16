b446ec3cb41d461c11f520876bf46bfeb40ab717
unknown <yonideserbaix@gmail.com>
2024-03-16 14:05:12 +0100

    update main section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Main content area</h1>
+    <main className="mx-auto max-w-6xl px-6">
+      <div className="px-10 pt-10">
+        <h1 className="text-4xl font-extrabold">Home Page</h1>
+        <p className="py-4 text-xl">The perfect landing page</p>
+      </div>
     </main>
   );
 }
