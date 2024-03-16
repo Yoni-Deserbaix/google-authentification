93b1c747118e4895d9206a8822c23bdc60f267f8
unknown <yonideserbaix@gmail.com>
2024-03-16 14:04:12 +0100

    create form

--- src/app/components/SiteHeader.tsx
+++ src/app/components/SiteHeader.tsx
@@ -5,7 +5,16 @@ export default function SiteHeader() {
   return (
     <header className="flex justify-between mx-4 max-w-7xl font-semibold items-center h-20 border-b bg-background">
       <div className="flex gap-4 items-center px-4">Next App!</div>
-      <p>Search area</p>
+      <form className="hidden md:inline-block">
+        <input
+          id="search"
+          name="search"
+          type="search"
+          autoComplete="off"
+          placeholder="Search something.."
+          className="h-9 lg:w-[300px] bg-background border-b-2 outline-none"
+        />
+      </form>
       <div className="flex gap-4">
         <p>About</p>
         <p>Services</p>
