eb9257923bdfb0af578fed3baa816b47f90e9a4b
unknown <yonideserbaix@gmail.com>
2024-03-16 11:26:30 +0100

    update style Header

--- src/app/components/SiteHeader.tsx
+++ src/app/components/SiteHeader.tsx
@@ -3,7 +3,7 @@ import ThemeToggle from "./ThemeToggle";
 
 export default function SiteHeader() {
   return (
-    <header className="flex justify-between mx-4 max-w-7xl font-semibold items-center h-20 border-b">
+    <header className="flex justify-between mx-4 max-w-7xl font-semibold items-center h-20 border-b bg-background">
       <div className="flex gap-4 items-center">
         <Image src="/next.svg" alt="logo" height={10} width={10} className="w-10 h-10"></Image>
       </div>
