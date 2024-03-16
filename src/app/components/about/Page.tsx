0ba3c1d5b429986987dcc17cbbe4e7435d76e18f
unknown <yonideserbaix@gmail.com>
2024-03-16 14:10:45 +0100

    create about page

---
+++ src/app/components/about/Page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <main className="mx-auto max-w-6xl px-6">
+      <div className="px-10 pt-10">
+        <h1 className="text-4xl font-extrabold">About Page</h1>
+        <p className="py-4 text-xl">About your company</p>
+      </div>
+    </main>
+  );
+}
