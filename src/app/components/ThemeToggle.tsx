629d9b17f024b54ab21e6a72ad477650e708edf5
unknown <yonideserbaix@gmail.com>
2024-03-16 11:29:24 +0100

    create ThemeToggle component

--- src/app/components/ThemeToggle.tsx
+++ src/app/components/ThemeToggle.tsx
@@ -1,3 +1,21 @@
+"use client";
+import { useTheme } from "next-themes";
+import { FiSun, FiMoon } from "react-icons/fi";
+
 export default function ThemeToggle() {
-    return <div></div>
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <div>
+      <button
+        className="flex items-center"
+        onClick={() => {
+          setTheme(theme === "light" ? "dark" : "light");
+          console.log(theme);
+        }}
+      >
+        {theme === "light" ? <FiMoon /> : <FiSun />}
+      </button>
+    </div>
+  );
 }
