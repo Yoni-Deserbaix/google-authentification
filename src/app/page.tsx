0d13832e2cf589ad73f0340d18bd1b44fff832ac
unknown <yonideserbaix@gmail.com>
2024-03-21 22:15:17 +0100

    update style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,15 @@ export default function Home() {
     <main className="mx-auto max-w-6xl px-6">
       <div className="px-10 pt-10">
         <h1 className="text-4xl font-extrabold">Home Page</h1>
-        <p className="py- text-xl">The perfect landing page</p>
+        <p className="py-16 text-xl">The perfect landing page</p>
+        <div className="flex flex-row gap-5 pt-10">
         <SignInButton />
-        <Button>
-          <a href="https://next-auth.js.org/" target="bla k">
-            Documentation
-          </a>
-        </Button>
+          <Button>
+            <a href="https://next-auth.js.org/" target="bla k">
+              Documentation
+            </a>
+          </Button>
+        </div>
       </div>
     </main>
   );
